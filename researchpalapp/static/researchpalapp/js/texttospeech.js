8d4f8ae40959bbab13d950e5e5232d1521fcf861
Akash <gsaisriram.akash@gmail.com>
2022-04-03 08:29:50 +0530

    JS for Pause and Play of the Speech done.

--- researchpalapp/static/researchpalapp/js/texttospeech.js
+++ researchpalapp/static/researchpalapp/js/texttospeech.js
@@ -1,1 +1,9 @@
 /* JS for text to speech */
+
+document.querySelector("#resume").addEventListener("click", () => {
+    window.speechSynthesis.resume();
+});
+
+document.querySelector("#pause").addEventListener("click", () => {
+    window.speechSynthesis.pause();
+});
